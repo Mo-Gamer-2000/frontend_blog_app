f4daabd5392b42f01d13f315f7cb0bb4c3b5c084
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-11 13:36:56 +0100

    Article card component created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,7 +71,7 @@ const Header = () => {
   };
 
   return (
-    <section>
+    <section className="sticky top-0 left-0 right-0 z-50 bg-white">
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img className="w-[112px]" src={images.Logo} alt="logo" />
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import Logo from "../assets/Logo.png";
 import HeroImage from "../assets/HeroImage.png";
+import Post1Image from "../assets/posts/post1.jpg";
+import PostProfileImage from "../assets/posts/post-profile.jpg";
 
 const images = {
   Logo,
   HeroImage,
+  Post1Image,
+  PostProfileImage,
 };
 
 export default images;
