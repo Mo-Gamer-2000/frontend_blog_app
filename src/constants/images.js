89c4c5cfb776a2438969424ef10fb91ad765268a
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-11 17:11:45 +0100

    CTA section created

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,14 @@ import Logo from "../assets/Logo.png";
 import HeroImage from "../assets/HeroImage.png";
 import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.jpg";
+import CtaImage from "../assets/CtaImage.jpg";
 
 const images = {
   Logo,
   HeroImage,
   Post1Image,
   PostProfileImage,
+  CtaImage,
 };
 
 export default images;
