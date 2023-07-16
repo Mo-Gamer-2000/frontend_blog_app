0e133df78c16f3db26f8f5776a5a84319c5c5264
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-16 23:41:02 +0100

    Save User Data in Local Storage - Stay Logged In

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,37 +1,18 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
-import { useSelector, useDispatch } from "react-redux";
 import ArticleCard from "../../../components/ArticleCard";
-import { changeCount } from "../../../store/actions/countActions";
 
 const Articles = () => {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
-
-  const countChangeHandler = (type) => {
-    dispatch(changeCount(type));
-  };
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
-      <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
+      <div className=" flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
       </div>
       <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
-        <span>More Articles</span>
+        <span>More articles</span>
         <FaArrowRight className="w-3 h-3" />
       </button>
-
-      {/* THIS IS A TEST*/}
-      <div className="mt-2 flex items-center gap-x-5">
-        <button onClick={() => countChangeHandler("INCREAMENT")}>
-          Increament
-        </button>
-        {count.number}
-        <button onClick={() => countChangeHandler("DECREAMENT")}>
-          Decreament
-        </button>
-      </div>
     </section>
   );
 };
