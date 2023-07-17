3b454aeb0238497230220ffc0f6d3015432a4178
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-17 12:33:21 +0100

    Login Functionality - Implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
 import RegisterPage from "./pages/register/RegisterPage";
+import LoginPage from "./pages/login/LoginPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
       <Toaster />
     </div>
