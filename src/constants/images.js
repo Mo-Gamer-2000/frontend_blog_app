997c084dbb1fb029ac69881098f008f04ff80090
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-21 15:43:50 +0100

    Render Posts in Frontend - HomePage

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,12 +4,17 @@ import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.jpg";
 import CtaImage from "../assets/CtaImage.jpg";
 
+const samplePostImage = "/images/sample.jpg";
+const userImage = "/images/user.png";
+
 const images = {
   Logo,
   HeroImage,
   Post1Image,
   PostProfileImage,
   CtaImage,
+  samplePostImage,
+  userImage
 };
 
 export default images;
