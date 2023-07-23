7939e3d564468f3dd1721afe33c58aebb089f0cb
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-23 20:09:47 +0100

    Implemented Loading & Error State for Posts

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -3,7 +3,9 @@ import { FaArrowRight } from "react-icons/fa";
 import ArticleCard from "../../../components/ArticleCard";
 import { useQuery } from "@tanstack/react-query";
 import { getAllPosts } from "../../../services/index/posts";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
+import ArticleCardSkeleton from "../../../components/ArticleCardSkeleton";
+import ErrorMessage from "../../../components/ErrorMessage";
 
 const Articles = () => {
   const { data, isLoading, isError } = useQuery({
@@ -11,25 +13,34 @@ const Articles = () => {
     queryKey: ["posts"],
     onError: (error) => {
       toast.error(error.message);
-
       console.log(error);
     },
   });
+
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
       <div className=" flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
-        {!isLoading &&
-          !isError &&
+        {isLoading ? (
+          [...Array(3)].map((item, index) => (
+            <ArticleCardSkeleton
+              key={index}
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+            />
+          ))
+        ) : isError ? (
+          <ErrorMessage message="Unable to fetch Post data" />
+        ) : (
           data.map((post) => (
             <ArticleCard
               key={post._id}
               post={post}
               className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
             />
-          ))}
+          ))
+        )}
       </div>
       <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
-        <span>More articles</span>
+        <span>More Articles</span>
         <FaArrowRight className="w-3 h-3" />
       </button>
     </section>
