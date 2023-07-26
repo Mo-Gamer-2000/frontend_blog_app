26698de87af42af92e67f5636a8c972c61ecf388
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-26 14:26:33 +0100

    Minor Change

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
             <input
               className="placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] md:py-4"
               type="text"
-              placeholder="Search article"
+              placeholder="Search Article"
             />
           </div>
           <button className="w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2">
