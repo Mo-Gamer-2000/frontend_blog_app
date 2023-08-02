bcc15aaa03c58251d0595d1a8071215c892ad4b6
Moeez Abdul <moeezrajput2000@gmail.com>
2023-08-02 12:54:30 +0100

    State 2: Logic Implemented

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,5 +1,7 @@
 import { useMemo } from "react";
 
+const DOTS = "...";
+
 export const usePagination = ({
   totalCount,
   pageSize,
@@ -14,6 +16,28 @@ export const usePagination = ({
     // State 1: If the number of pages are less then page numbers
     if (totalPageNumbers >= totalPageCount) {
       return range(1, totalPageCount);
+    }
+
+    // Calculating the Left & Right sibling index
+    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
+    const rightSiblingIndex = Math.min(
+      currentPage + siblingCount,
+      totalPageCount
+    );
+
+    // Calculating that weather if we want to show left dots, right dots or both of them
+    // We do not show dots just when there is just one page number to be inserted between the sibling and the page limit
+    const shouldShowLeftDots = leftSiblingIndex > 2;
+    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+
+    const firstPageIndex = 1;
+    const lastPageIndex = totalPageCount;
+
+    // State 2: No left dots to show, but right fots to be visible
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      let leftItemCount = 3 + 2 * siblingCount;
+      let leftRange = range(1, leftItemCount);
+      return [...leftRange, DOTS, totalPageCount];
     }
   }, [totalCount, pageSize, siblingCount, currentPage, totalPageCount]);
 
