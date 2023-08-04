8fe2886bc769ac34cfec5cfcf17d9d6dc120aae6
Moeez Abdul <moeezrajput2000@gmail.com>
2023-08-04 16:09:00 +0100

    Edit Post Func/Page in Progress

---
+++ src/pages/admin/screens/posts/EditPost.jsx
@@ -0,0 +1,89 @@
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { getSinglePost } from "../../../../services/index/posts";
+import { Link, useParams } from "react-router-dom";
+import ArticleDetailSkeleton from "../../../articleDetail/components/ArticleDetailSkeleton";
+import ErrorMessage from "../../../../components/ErrorMessage";
+import parseJsonToHtml from "../../../../utils/parseJsonToHtml";
+import { stables } from "../../../../constants";
+import { HiOutlineCamera } from "react-icons/hi";
+
+const EditPost = () => {
+  const { slug } = useParams();
+  const [initialPhoto, setInitialPhoto] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [body, setBody] = useState(null);
+
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => getSinglePost({ slug }),
+    queryKey: ["blog", slug],
+  });
+
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      setInitialPhoto(data?.photo);
+      setBody(parseJsonToHtml(data?.body));
+    }
+  }, [data, isError, isLoading]);
+
+  const handleFileChange = (e) => {
+    const file = e.target.file[0];
+    setPhoto(file);
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Unable to Fetch the Post Data" />
+      ) : (
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-1">
+            <label htmlFor="postPicture" className="w-full cursor-pointer">
+              {photo ? (
+                <img
+                  src={URL.createObjectUR(photo)}
+                  alt={data?.title}
+                  className="rounded-xl w-full"
+                />
+              ) : initialPhoto ? (
+                <img
+                  src={stables.UPLOAD_FOLDER_BASE_URL + data?.photo}
+                  alt={data?.title}
+                  className="rounded-xl w-full"
+                />
+              ) : (
+                <div className="w-full h-full bg-blue-50/50 flex justify-center items-center">
+                  <HiOutlineCamera className="w-7 h-auto text-primary" />
+                </div>
+              )}
+            </label>
+            <input
+              type="file"
+              className="sr-only"
+              id="postPicture"
+              onChange={handleFileChange}
+            />
+            <div className="mt-4 flex gap-2">
+              {data?.categories.map((category) => (
+                <Link
+                  to={`/blog?category=${category.name}`}
+                  className="text-primary text-sm font-roboto inline-block md:text-base"
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+            <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
+              {data?.title}
+            </h1>
+            <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+          </article>
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default EditPost;
