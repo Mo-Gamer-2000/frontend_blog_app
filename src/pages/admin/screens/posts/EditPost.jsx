72351a903ee2776dc44e9742890f006dd060242f
Moeez Abdul <moeezrajput2000@gmail.com>
2023-08-05 11:48:59 +0100

    Update Post Func Implemented (Admin Panel)

--- src/pages/admin/screens/posts/EditPost.jsx
+++ src/pages/admin/screens/posts/EditPost.jsx
@@ -1,15 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
-import { getSinglePost } from "../../../../services/index/posts";
+import { getSinglePost, updatePost } from "../../../../services/index/posts";
 import { Link, useParams } from "react-router-dom";
 import ArticleDetailSkeleton from "../../../articleDetail/components/ArticleDetailSkeleton";
 import ErrorMessage from "../../../../components/ErrorMessage";
 import parseJsonToHtml from "../../../../utils/parseJsonToHtml";
 import { stables } from "../../../../constants";
 import { HiOutlineCamera } from "react-icons/hi";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 const EditPost = () => {
   const { slug } = useParams();
+  const queryClient = useQueryClient();
+  const userState = useSelector((state) => state.user);
   const [initialPhoto, setInitialPhoto] = useState(null);
   const [photo, setPhoto] = useState(null);
   const [body, setBody] = useState(null);
@@ -17,6 +21,27 @@ const EditPost = () => {
   const { data, isLoading, isError } = useQuery({
     queryFn: () => getSinglePost({ slug }),
     queryKey: ["blog", slug],
+  });
+
+  const {
+    mutate: mutateUpdatePostDetail,
+    isLoading: isLoadingUpdatePostDetail,
+  } = useMutation({
+    mutationFn: ({ updatedData, slug, token }) => {
+      return updatePost({
+        updatedData,
+        slug,
+        token,
+      });
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["blog", slug]);
+      toast.success("Post is Updated");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
   });
 
   useEffect(() => {
@@ -27,8 +52,36 @@ const EditPost = () => {
   }, [data, isError, isLoading]);
 
   const handleFileChange = (e) => {
-    const file = e.target.file[0];
+    const file = e.target.files[0];
     setPhoto(file);
+  };
+
+  const handleUpdatePost = async () => {
+    let updatedData = new FormData();
+
+    if (!initialPhoto && photo) {
+      updatedData.append("postPicture", photo);
+    } else if (initialPhoto && !photo) {
+      const urlToObject = async (url) => {
+        let response = await fetch(url);
+        let blob = await response.blob();
+        const file = new File([blob], initialPhoto, { type: blob.type });
+        return file;
+      };
+      const picture = await urlToObject(
+        stables.UPLOAD_FOLDER_BASE_URL + data?.photo
+      );
+
+      updatedData.append("postPicture", picture);
+    }
+
+    updatedData.append("document", JSON.stringify({}));
+
+    mutateUpdatePostDetail({
+      updatedData,
+      slug,
+      token: userState.userInfo.token,
+    });
   };
 
   return (
@@ -43,7 +96,7 @@ const EditPost = () => {
             <label htmlFor="postPicture" className="w-full cursor-pointer">
               {photo ? (
                 <img
-                  src={URL.createObjectUR(photo)}
+                  src={URL.createObjectURL(photo)}
                   alt={data?.title}
                   className="rounded-xl w-full"
                 />
@@ -54,7 +107,7 @@ const EditPost = () => {
                   className="rounded-xl w-full"
                 />
               ) : (
-                <div className="w-full h-full bg-blue-50/50 flex justify-center items-center">
+                <div className="w-full min-h-[200px] bg-blue-50/50 flex justify-center items-center">
                   <HiOutlineCamera className="w-7 h-auto text-primary" />
                 </div>
               )}
@@ -79,6 +132,14 @@ const EditPost = () => {
               {data?.title}
             </h1>
             <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+            <button
+              disabled={isLoadingUpdatePostDetail}
+              type="button"
+              onClick={handleUpdatePost}
+              className="w-full bg-green-500 text-white font-semibold rounded-lg px-4 py-2 disabled:cursor-not-allowed opacity-70"
+            >
+              Update Post
+            </button>
           </article>
         </section>
       )}
