53dae88aeee9cad579491e3b905a5abd31b416e4
Moeez Abdul <moeezrajput2000@gmail.com>
2024-02-15 13:06:24 +0000

    Added UserId at the end of our End point as a Params - Implemented Input to change Admin Status of the Users Via Admin Panel - Major Bug fixed with check box not matching admin status value - FIXED

--- src/pages/admin/screens/users/Users.jsx
+++ src/pages/admin/screens/users/Users.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { images, stables } from "../../../../constants";
-import { deleteUser, getAllUsers } from "../../../../services/index/users";
-import Pagination from "../../../../components/Pagination";
+import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
-import { Link } from "react-router-dom";
+import { images, stables } from "../../../../constants";
+import {
+  deleteUser,
+  getAllUsers,
+  updateProfile,
+} from "../../../../services/index/users";
 import { useDataTable } from "../../../../hooks/useDataTable";
 import DataTable from "../../components/DataTable";
 
@@ -33,6 +36,38 @@ const Users = () => {
       });
     },
   });
+
+  const { mutate: mutateUpdateUser, isLoading: isLoadingUpdateUser } =
+    useMutation({
+      mutationFn: ({ isAdmin, userId }) => {
+        return updateProfile({
+          token: userState.userInfo.token,
+          userData: { admin: isAdmin },
+          userId,
+        });
+      },
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(["users"]);
+        toast.success("User is updated");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+        console.log(error);
+      },
+    });
+
+  const handleAdminCheck = (event, userId) => {
+    const initialCheckValue = !event.target.checked;
+    if (
+      window.confirm(
+        "Are you sure you want to change the admin status of this user?"
+      )
+    ) {
+      mutateUpdateUser({ isAdmin: event.target.checked, userId });
+    } else {
+      event.target.checked = initialCheckValue;
+    }
+  };
 
   return (
     <DataTable
@@ -98,9 +133,13 @@ const Users = () => {
             </p>
           </td>
           <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-            <p className="text-gray-900 whitespace-no-wrap">
-              {user.admin ? "✅ Verified" : "❌ Not verified"}
-            </p>
+            <input
+              type="checkbox"
+              className="d-checkbox disabled:bg-orange-400 disabled:opacity-100 checked:bg-[url('../public/images/check.png')] bg-cover checked:disabled:bg-none"
+              defaultChecked={user.admin}
+              onChange={(event) => handleAdminCheck(event, user._id)}
+              disabled={isLoadingUpdateUser}
+            />
           </td>
           <td className="px-5 py-5 text-sm bg-white border-b border-gray-200 space-x-5">
             <button
